421568bb42871d4a7fdceb8b0bffc76592fa94d4
aralhibshi1 <alhibshi@live.com>
2023-01-29 14:09:40 +0300

    Ingredient, Order, and User APIs. Address Model

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,51 @@
 const User = require("../models/User")
 
+// // TEMP HTTP POST - User Create
+// exports.user_create_post = (req, res) => {
+//     let user = new User(req.body)
+    
+//     user.save()
+//     .then(user => {
+//         res.json({user})
+//     })
+//     .catch(err => {
+//         console.log(err)
+//         console.log('Cannot Create User')
+//     })
+// }
+
+// HTTP GET - User Details
 exports.user_details_get = (req,res) => {
-    res.json({"message":"user_details_get works"})
+    User.findById(req.body._id)
+    .then(user => {
+        res.json({user})
+    })
+    .catch(err => {
+        console.log(err)
+        console.log('Cannot Get User Details')
+    })
 }
 
+// HTTP PUT - User Update
 exports.user_update_put = (req,res) => {
-    res.json({"message":"user_update_put works"})
+    User.findByIdAndUpdate(req.body._id, req.body, {new: true})
+    .then(user => {
+        res.json({user})
+    })
+    .catch(err => {
+        console.log(err)
+        console.log('Cannot Update User')
+    })
 }
 
+// HTTP DELETE - User Delete
 exports.user_drop_delete = (req,res) => {
-    res.json({"message":"user_drop_delete works"})
+    User.findByIdAndDelete(req.body._id)
+    .then(user => {
+        res.json({user})
+    })
+    .catch(err => {
+        console.log(err)
+        console.log('Cannot Delete User')
+    })
 }
